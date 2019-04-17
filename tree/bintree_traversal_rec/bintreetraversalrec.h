019c0a4c8abf6d5d4b98d2a7911ed99ef2aa3640
Taewoo Lee <lteawoo@naver.com>
2019-04-17 22:13:40 +0900

    BinaryTree Traversal Recursive 방법

---
+++ tree/bintree_traversal_rec/bintreetraversalrec.h
@@ -0,0 +1,11 @@
+#ifndef _BIN_TREE_TRAVERSAL_RECURSIVE
+#define _BIN_TREE_TRAVERSAL_RECURSIVE
+
+void preorderTraversalRecursiveBinTree(BinTree *pBinTree);
+void preorderTraversalRecursiveBinTreeNode(BinTreeNode *pRootNode);
+void inorderTraversalRecursiveBinTree(BinTree *pBinTree);
+void inorderTraversalRecursiveBinTreeNode(BinTreeNode *pRootNode);
+void postorderTraversalRecursiveBinTree(BinTree *pBinTree);
+void postorderTraversalRecursiveBinTreeNode(BinTreeNode *pRootNode);
+
+#endif
