e42192f40e9738d8ce7b62f09a15350a0aebdfc3
Taewoo Lee <lteawoo@naver.com>
2019-06-27 14:01:51 +0900

    그래프 Adjacency matrix 구현

--- graph/arraygraph/arraygraph.c
+++ graph/arraygraph/arraygraph.c
@@ -56,3 +56,122 @@ ArrayGraph* createArrayGraph(int maxVertexCount) {
 
 	return pReturn;
 }
+
+ArrayGraph* createArrayDirectedGraph(int maxVertexCount)
+{
+	ArrayGraph* pReturn = NULL;
+	pReturn = createArrayGraph(maxVertexCount);
+	if (pReturn != NULL) {
+		pReturn->graphType = GRAPH_DIRECTED;
+	}
+
+	return pReturn;
+}
+
+int addVertexAG(ArrayGraph* pGraph, int vertexID)
+{
+	int ret = SUCCESS;
+
+	if (pGraph != NULL) {
+		if (vertexID < pGraph->maxVertexCount) {
+			if (pGraph->pVertex[vertexID] == NOT_USED) {
+				pGraph->pVertex[vertexID] = USED;
+				pGraph->currentVertexCount++;
+			}
+			else {
+				printf("오류, 이미 존재하는 노드[%d],in addVertexAG()\n", vertexID);
+				ret = FAIL;
+			}
+		}
+		else {
+			printf("오류, 최대 노드 개수 초과 [%d], in addVertexAG()\n", pGraph->maxVertexCount);
+			ret = FAIL;
+		}
+	}
+
+	return ret;
+}
+
+int addEdgewithWeightAG(ArrayGraph* pGraph, int fromVertexID, int toVertexID, int weight) {
+	int ret = SUCCESS;
+
+	if (pGraph != NULL) {
+		if (checkVertexValid(pGraph, fromVertexID) != SUCCESS) {
+			ret = FAIL;
+			return ret;
+		}
+
+		if (checkVertexValid(pGraph, toVertexID) != SUCCESS) {
+			ret = FAIL;
+			return ret;
+		}
+		pGraph->ppAdjEdge[fromVertexID][toVertexID] = weight;
+
+		if (pGraph->graphType == GRAPH_UNDIRECTED) {
+			pGraph->ppAdjEdge[toVertexID][fromVertexID] = weight;
+		}
+	}
+
+	return ret;
+}
+
+int checkVertexValid(ArrayGraph* pGraph, int vertexID) {
+	int ret = SUCCESS;
+
+	if (vertexID >= pGraph->maxVertexCount || pGraph->pVertex[vertexID] == NOT_USED) {
+		printf("오류, 노드 정보[%d]\n", vertexID);
+		ret = FAIL;
+	}
+
+	return ret;
+}
+
+int addEdgeAG(ArrayGraph* pGraph, int fromVertexID, int toVertexID)
+{
+	return addEdgewithWeightAG(pGraph, fromVertexID, toVertexID, USED);
+}
+
+int removeEdgeAG(ArrayGraph* pGraph, int fromVertexID, int toVertexID) {
+	int ret = SUCCESS;
+
+	if (pGraph != NULL) {
+		if (checkVertexValid(pGraph, fromVertexID) != SUCCESS) {
+			ret = FAIL;
+			return ret;
+		}
+
+		if (checkVertexValid(pGraph, toVertexID) != SUCCESS) {
+			ret = FAIL;
+			return ret;
+		}
+
+		pGraph->ppAdjEdge[fromVertexID][toVertexID] = 0;
+
+		if (pGraph->graphType == GRAPH_UNDIRECTED) {
+			pGraph->ppAdjEdge[toVertexID][fromVertexID] = 0;
+		}
+	}
+
+	return ret;
+}
+
+int removeVertexAG(ArrayGraph* pGraph, int vertexID) {
+	int ret = SUCCESS;
+	int i = 0;
+
+	if (pGraph != NULL) {
+		if (checkVertexValid(pGraph, vertexID) != SUCCESS) {
+			ret = FAIL;
+			return ret;
+		}
+
+		for (i = 0; i < pGraph->maxVertexCount; i++) {
+			removeEdgeAG(pGraph, vertexID, i);
+			removeEdgeAG(pGraph, i, vertexID);
+		}
+
+		pGraph->pVertex[vertexID] = NOT_USED;
+	}
+
+	return ret;
+}
