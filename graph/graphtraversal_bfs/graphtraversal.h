985d6bc7b5af9a0cd824cd18f34fc2c26f97eb51
Taewoo Lee <lteawoo@naver.com>
2019-07-06 15:38:34 +0900

    Breadth First Search

---
+++ graph/graphtraversal_bfs/graphtraversal.h
@@ -0,0 +1,7 @@
+#ifndef _GRAPH_TRAVERSAL_
+#define _GRAPH_TRAVERSAL_
+
+void traversalDFS(LinkedGraph* pGraph, int startVertexID);
+void traversalBFS(LinkedGraph* pGraph, int startVertexID);
+
+#endif
